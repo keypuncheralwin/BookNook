47c2da02746b7a9000d7b41e23b27dc1994063ca
Alwin George <alwingeorge11@gmail.com>
2023-10-15 20:12:36 +1030

    Implement basic dark mode with tailwind

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from customers.models import Customer
 
+def change_theme(request):
+    if 'is_dark_mode' in request.session:
+        request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    else:
+        request.session['is_dark_mode'] = False
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 def home_view(request):
     qs = Customer.objects.all()
