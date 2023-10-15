367bce2f33ef38ee5c5f3b8bfff64a522ad684fd
Alwin George <alwingeorge11@gmail.com>
2023-10-15 20:35:52 +1030

    Add a basic navbar and sidebar

--- src/core/views.py
+++ src/core/views.py
@@ -6,8 +6,8 @@ def change_theme(request):
     if 'is_dark_mode' in request.session:
         request.session['is_dark_mode'] = not request.session['is_dark_mode']
     else:
-        request.session['is_dark_mode'] = False
-    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+        request.session['is_dark_mode'] = True
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def home_view(request):
     qs = Customer.objects.all()
