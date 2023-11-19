72c609d838ea7047bd661d8a912482ae9018e92e
Alwin George <alwingeorge11@gmail.com>
2023-11-19 19:01:18 +1030

    Initialize dashboard page

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from customers.models import Customer
+from django.views.generic import TemplateView
 
 def change_theme(request):
     if 'is_dark_mode' in request.session:
@@ -9,6 +10,8 @@ def change_theme(request):
         request.session['is_dark_mode'] = True
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-def home_view(request):
-    qs = Customer.objects.all()
-    return render(request, 'main.html', {'qs': qs})
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
+
+def chart_data(request):
+    return JsonResponse({'msg': 'Test chart data'})
