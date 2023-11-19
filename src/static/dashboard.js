80bcf4d3d80d9e278f61bc16ad1176e6da3addf4
Alwin George <alwingeorge11@gmail.com>
2023-11-19 20:32:34 +1030

    Render charts in dashboard using chart js

--- src/static/dashboard.js
+++ src/static/dashboard.js
@@ -6,9 +6,39 @@ $.ajax({
   type: "GET",
   url: "/chart-data",
   success: (resp) => {
-    const { msg } = resp;
-    console.log(msg);
-    dashboardBox.innerHTML = `<b>${msg}</b>`;
+    const { data } = resp;
+    for (let i = 0; i < data.length; i++) {
+      const ctx = document.getElementById(`chart-box${i + 1}`);
+      const descriptionBox = document.getElementById(`desc-box${i + 1}`);
+      const { labels, data: d, description, type } = data[i];
+      console.log(labels, d, description);
+      descriptionBox.innerHTML = description;
+
+      new Chart(ctx, {
+        type: type,
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              data: d,
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          plugins: {
+            legend: {
+              display: type === "pie" && true,
+            },
+          },
+        },
+      });
+    }
   },
   error: (err) => console.log(err),
 });
